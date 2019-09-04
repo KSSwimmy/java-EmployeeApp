22d7b2f35bb84c40b3ec65b80b71e822a822f841
Lambda_School_Loaner_163 <lambda_school_loaner_163@Lambda-School-Loaner-163s-MacBook-Pro.local>
2019-09-04 00:43:15 -0400

    :construction: gotta compile still added more code in main
    gotta compile still added more code in main

--- EmployeeAPP-Guided Project/src/main/java/com/lambdashool/employeeapp/Main.java
+++ EmployeeAPP-Guided Project/src/main/java/com/lambdashool/employeeapp/Main.java
@@ -2,10 +2,30 @@ package com.lambdashool.employeeapp;
 
 public class Main
 {
+    public static void workWithData()
+    {
+        // when we make an object of the class (ex: Employee and Company) it's called instantiation
+        // in the class' object's parameter the fields are listed in the order of each class' constructor (provided examples above each object)
+
+        // HeathPlan's constructor - public HealthPlan(String name)
+        HealthPlan h1 = new HealthPlan("My Health Plan 1");
+        HealthPlan h2 = new HealthPlan("My Heath Plan 2");
+
+        // Company's constructor - public Company(String name, int debt)
+        Company c1 = new Company("Company A", 100);
+
+        // Employee's constructor - public Employee(String fname, String lname, double salary, boolean has401k, int companyId, int healthPlainId)
+        Employee epm1 = new Employee("Steve", "Green", 45000, true, c1.id, h1.getId());
+        Employee epm2 = new Employee("May", "Ford", 80000, true, c1.id, h2.getId());
+
+        System.out.println("*** Query Data ***");
+        System.out.println();
+    }
+
    //psvm (hotkey for "public static void main)
    public static void main(String[] args)
                                     //arguments
    {
-
+        workWithData();
    }
 }
